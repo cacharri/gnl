b203ecb71ed262e8364167e8329abbbc3e5a88eb
Inigo Alvarez echarri <ialvarez@c1r12s3.42madrid.com>
2021-05-05 21:38:16 +0200

    almost

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 16:12:56 by ialvarez          #+#    #+#             */
-/*   Updated: 2021/05/04 16:47:43 by ialvarez         ###   ########.fr       */
+/*   Updated: 2021/05/05 21:37:25 by ialvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,48 @@ int	get_next_line(int fd, char **line)
 	static	char *reader[4096];
 	char	rest[BUFFER_SIZE + 1];
 	char	*tempor;
-	char	*temporrr;
+	char	*aux;
 	int		i;
 
 	if (BUFFER_SIZE < 1 || !line || fd < 0)
 		return (-1);
-	/*i = read(fd, rest, BUFFER_SIZE);
-	rest[i] = '\0';
-	printf("yahacomooovengoyoo->>>>%s\n", rest);
-	if (!reader[fd])
-		reader[fd] = ft_strdup(rest);
-	printf("yahorayeeees->>>>%s\n", reader[fd]);
-	printf("yahacomooovengoyoo->>>>%s\n", rest);*/
 	i = read(fd, rest, BUFFER_SIZE);
+	printf("%lu\n", sizeof(rest));
+	/*if (ft_strchr(reader[fd], '\n') && i == 0)
+	{
+		i = 1;
+	}*/
+
 	while(i > 0)
 	{
 		rest[i] = '\0';
 		if (!reader[fd])
 			reader[fd] = ft_strdup(rest);
-		if (reader[fd])
-			reader[fd] = ft_strjoin(reader[fd], rest);
-		//printf("yahorayeeees->>>>%s\n", reader[fd]);
-		if ((tempor = ft_strchr(reader[fd], '\n')))
+		else
+		{
+			aux = ft_strjoin(reader[fd], rest);
+			free(reader[fd]);
+			reader[fd] = aux;
+		}
+		tempor = ft_strchr(reader[fd], '\n');
+		if (tempor)
 		{
 			*tempor = '\0';
 			*line = ft_strdup(reader[fd]);
-		//	printf("nunyestuuu%s\n", *line); 
-			temporrr = ft_strdup(++tempor);
-		//	printf("presumisdeserbuenmozuu--->%s\n", temporrr);
+			aux = ft_strdup(++tempor);
 			free(reader[fd]);
-			reader[fd] = temporrr;
-			free(temporrr);
+			reader[fd] = aux;
 
-			//printf("acomerlo---->%s\n", reader[fd]);
-			//temporrr = ft_strchr(rest, '\n');
-		//	reader[fd] = ft_strjoin(reader[fd], rest);
-		//	printf("acomerlo---->%s\n", reader[fd]);
-		//	printf("alo---->%s\n", *line);
-		//	printf("reader: |%s|\n", reader[fd]);
 			return (1);
 		}
+
 		i = read(fd, rest, BUFFER_SIZE);
 	}
+	/*if (ft_strchr(reader[fd], '\n') && i == 0)
+	{
+		i = 1;
+		return (1);
+	}*/
 	return (i);
 }
 
@@ -79,8 +79,11 @@ int main()
 		printf("LINEA ___MAIN->%s\n", line);
 		free(line);
 		n =  get_next_line(fd, &line);
+		printf("N___MAIN->%d\n", n);
+
 	}
-	
+	while(1)
+			 
 //	printf("LINEA ___MAIN11->%s\n", line);
 //	free(line);
     //fd = open ("texto.txt", O_RDWR);
@@ -104,3 +107,4 @@ int main()
     //free(line);
     //printf("%s\n", line);
 }
+
